6733e28d6c33bb1765ebcc0c4fa93c7b4edf952b
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-10-29 19:20:28 +0200

    type script environment

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// to declare a variable in TypeScript, you can use the `let`, `const`, or `var` keywords.
+// `let` allows you to declare a variable that can be reassigned later.
+// `const` is used for variables that should not be reassigned.
+// `var` is an older way to declare variables, but it's generally recommended to use `let` or `const` in modern TypeScript.
+
+let age: number =25;
+if(age < 50)
+    age +=10
