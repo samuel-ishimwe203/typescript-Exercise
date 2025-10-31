e34beb1b599180efffd487b5be9035549fd35d0f
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-10-31 15:17:35 +0200

    TypeScript class

--- public/app.js
+++ public/app.js
@@ -1,5 +1,23 @@
+//  TypeScript class
+class Invoice {
+    constructor(a, b, c) {
+        this.client = a;
+        this.details = b;
+        this.amount = c;
+    }
+    greet() {
+        return `${this.client} owns $${this.amount} for ${this.details}`;
+    }
+}
+const invoiceOne = new Invoice('samuel', 'kigali-nyagatare', 400);
+const invoiceTwo = new Invoice('Elie', 'Gasabo-nyagatare', 1000);
+let invoices = [];
+invoices.push(invoiceOne);
+invoices.push(invoiceTwo);
+console.log(invoices);
 // form submission event listener
 const form = document.querySelector('.new-item-form');
+// input 
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#toFrom');
 const details = document.querySelector('#details');
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,35 @@
+//  TypeScript class
+
+class Invoice {
+    // client:string;
+    // details:string;
+    // amount:number;
+
+    constructor(
+         readonly client:string,
+         private details:string,
+        public amount:number,
+    ){}
+
+    greet(){
+return `${this.client} owns $${this.amount} for ${this.details}`
+    }
+}
+
+const invoiceOne= new Invoice('samuel','kigali-nyagatare',400); 
+const invoiceTwo= new Invoice('Elie','Gasabo-nyagatare',1000); 
+
+let invoices:Invoice[]=[];
+invoices.push(invoiceOne)
+invoices.push(invoiceTwo)
+
+console.log(invoices);
+
 
 // form submission event listener
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-
+ // input 
 const type= document.querySelector('#type') as HTMLSelectElement;
 const toFrom= document.querySelector('#toFrom') as HTMLInputElement;
 const details= document.querySelector('#details') as HTMLInputElement;
