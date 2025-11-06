0e0a8d9d7d37d4c2c838a18a7082aff1a52069fe
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-11-06 22:51:15 +0200

    Typescript enums

---
+++ src/enums.ts
@@ -0,0 +1,23 @@
+// enums in typescript 
+
+enum role{
+    Admin,
+    User,
+    Guest
+    }
+
+
+    function getRole(user:role){
+        if(user===role.Admin){
+            console.log('you are allowed to enter to this site')
+        }
+        else if(user===role.User){
+            console.log("you are allowed to enter but there is something that you can't access")
+        }
+        else {
+            console.log('you arn not allowed to enter here please')
+        }
+    }
+
+
+    console.log(getRole(role.Admin))
