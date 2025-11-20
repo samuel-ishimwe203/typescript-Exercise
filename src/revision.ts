5138def51c1e4f8a151da2039eb2f7fef3e7c21e
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-11-20 12:22:24 +0200

    .

--- src/revision.ts
+++ src/revision.ts
@@ -261,6 +261,32 @@ processRecipe({
 });
 
 
+// Update the function definition so that all tests pass
+ function move(direction:'up'| 'down' | 'left' | 'right', distance: number) {}
+
+// TESTS
+
+move('up', 10);
+move('left', 5);
+
+move(
+  // @ts-expect-error - "up-right" is not a valid direction
+  'up-right',
+  10
+);
+
+move(
+  // @ts-expect-error - "down-left" is not a valid direction
+  'down-left',
+  20
+);
+
+move(
+  'up',
+  // @ts-expect-error - "20" is not a valid distance
+  '20'
+);
+
 
 
 
