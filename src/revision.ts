aa3c90cd20032d212798879ff65c60dca742c401
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-11-20 12:45:45 +0200

    .

--- src/revision.ts
+++ src/revision.ts
@@ -289,7 +289,38 @@ move(
 
 
 
+/*6. The following function is meant to log the city for a customer’s 
+     shipping address. However, the code is unsafe and may throw runtime errors
+    due to missing or undefined properties.Without changing the logic of the function,
+    **fix the typing issues** and **make the function safe** using proper TypeScript types. */
 
+    interface Address{
+        city:string;
+        street?:string
+    }
+
+    interface Customer{
+        name:string;
+        address:Address;
+    }
+
+    interface Order{
+        customer:Customer;
+    }
+const logOrder = (order: Order) => {
+  console.log(`Shipping to: ${order.customer.address.city}`);
+};
+
+logOrder({
+  customer: {
+    name: 'Jane',
+    // address is missing
+    address:{
+        city:"Nyagatare"
+    }
+    
+  }
+});
 
  
 
