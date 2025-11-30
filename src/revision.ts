763e10b463d1d411152b645ffabe8964b0fb6663
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-11-30 22:22:33 +0200

    generic type

--- src/revision.ts
+++ src/revision.ts
@@ -594,3 +594,36 @@ function move(direction:"north"| "south" | "east" |"west") {
 
 
 console.log(move("north"));
+
+
+
+// 
+
+ class BankAccount {
+  // Your code here
+  private balance: number;
+
+  constructor(initialBalance: number) {
+    this.balance = initialBalance;
+      }
+
+  deposit(amount: number): void {
+    this.balance += amount;
+  }   
+
+  withdraw(amount: number): void {
+    if (amount <= this.balance) {
+      this.balance -= amount;   
+    } else {
+      console.log("Insufficient funds");
+    }
+  }
+  getBalance(): number {
+    return this.balance;
+  } 
+}
+
+const account = new BankAccount(1000);
+account.deposit(500);
+account.withdraw(200);
+console.log(account.getBalance()); 
