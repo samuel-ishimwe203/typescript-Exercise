ffe9807616cea3d4d05759bfadbf072451fdb4a2
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-05 21:32:02 +0200

    chore: interface

--- src/revision.ts
+++ src/revision.ts
@@ -851,7 +851,7 @@ function indexProductsById(arr:Products[]){
 const productss = [
   { id: 1, name: "Laptop", price: 999 },
   { id: 2, name: "Mouse", price: 25 }
-]
+];
 
 const indexed = indexProductsById(productss);
 console.log(indexed)
